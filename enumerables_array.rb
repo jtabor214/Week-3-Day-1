02900f5da43837eeff0ce5b1633f1005206ece96
Jacob Tabor <jtabor214@gmail.com>
2023-09-04 23:07:46 -0400

    completed my_select

--- enumerables_array.rb
+++ enumerables_array.rb
@@ -10,24 +10,23 @@ class Array
         self
     end 
 
+    def my_select(&block)
+    array = []
+        self.my_each do |ele|
+            if block.call(ele)
+                array << ele 
+            end
+        end
+        array
+    end
     
 
 
 end
 
-return_value = [1, 2, 3].my_each do |num|
-    puts num
-   end.my_each do |num|
-    puts num
-   end
-   # => 1
-   #    2
-   #    3
-   #    1
-   #    2
-   #    3
-   
-   p return_value  # => [1, 2, 3]
 
+a = [1, 2, 3]
+p a.my_select { |num| num > 1 } # => [2, 3]
+p a.my_select { |num| num == 4 } # => []
 
    
