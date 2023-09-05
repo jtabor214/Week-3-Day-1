efc7b90b7a5e69fe1aefd5937afc2e8cb2f8ab89
Jacob Tabor <jtabor214@gmail.com>
2023-09-04 23:58:47 -0400

    attempt at my_zip

--- enumerables_array.rb
+++ enumerables_array.rb
@@ -64,10 +64,28 @@ class Array
         flattened
     end
 
+    def my_zip(*arg)
+    array = Array.new(self.length) {Array.new(arg.length + 1)}
+
+        arg.each_with_index do |subArr, idx|
+            array <<  subArr[idx]
+        end
+
+        array
+    end
+
+
 end
 
+a = [ 4, 5, 6 ]
+b = [ 7, 8, 9 ]
+p [1, 2, 3].my_zip(a, b) # => [[1, 4, 7], [2, 5, 8], [3, 6, 9]]
+p a.my_zip([1,2], [8])   # => [[4, 1, 8], [5, 2, nil], [6, nil, nil]]
+p [1, 2].my_zip(a, b)    # => [[1, 4, 7], [2, 5, 8]]
 
-p [1, 2, 3, [4, [5, 6]], [[[7]], 8]].my_flatten # => [1, 2, 3, 4, 5, 6, 7, 8]
+c = [10, 11, 12]
+d = [13, 14, 15]
+p [1, 2].my_zip(a, b, c, d)    # => [[1, 4, 7, 10, 13], [2, 5, 8, 11, 14]]
 
 
    
